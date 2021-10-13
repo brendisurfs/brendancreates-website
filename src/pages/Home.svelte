6677239c91e1e6dfe4069fe8d5605abd585996c8
brendisurfs <brendan.prednis@protonmail.com>
2021-10-13 09:53:20 -0700

    fixed up home message on mobile that switches to desktop.
    Signed-off-by: brendisurfs <brendan.prednis@protonmail.com>

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -2,18 +2,33 @@
     import Canvas from '../components/Canvas.svelte';
     import {fade, fly} from "svelte/transition"
 
+    let isMobileState: boolean;
     let message: string = 'something 3D will be here soon.';
+    let mobileMessage: string = "this experience is much better on desktop."
     let messageLength = message.length;
-
     console.log(messageLength);
+
+    // isMobile state
+    if (/android|webOS|iPhone|iPad/i.test(navigator.userAgent)) {
+        isMobileState = true;
+    } else {
+        isMobileState = false;
+    }
+
 </script>
 
 <!-- <Canvas /> -->
 <div class="container" in:fly="{{y: 200, duration: 200, delay: 100 }}" out:fade="{{duration: 100}}">
 
-    <div class="typewriter">
-        <p>something 3D will be here soon.</p>
+    {#if isMobileState}
+    <div class="mobileType">
+        <p>This experience is much better on desktop.</p>
     </div>
+    {:else}
+    <div class="typewriter">
+            <p>something 3D will be here soon.</p>
+        </div>
+    {/if}
 </div>
 
 <style>
@@ -46,6 +61,28 @@
         animation: typing 2.5s steps(30, end),
             blink-caret 0.75s step-end infinite;
     }
+    .mobileType {
+        position: relative;
+        text-align: center;
+        align-items: center;
+        display: flex;
+        justify-content: center;
+        height: 50vh;
+        transform: translate(0em, -5em);
+    }
+
+
+    .mobileType p {
+        /* width: 18ch; */
+        margin: 0 auto;
+        overflow: hidden;
+        color: whitesmoke;
+        white-space: nowrap;
+        border-right: 0.5em solid black;
+        animation: typing-mobile 2.5s steps(30, end),
+            blink-mobile 0.75s step-end infinite;
+    }
+
 
     /* typing effect */
     @keyframes typing {
@@ -53,12 +90,29 @@
             width: 0;
         }
         to {
-            width: 26ch;
+            width: 27ch;
+        }
+    }
+    @keyframes typing-mobile {
+        from {
+            width: 0;
+        }
+        to {
+            width: 32ch;
         }
     }
 
     /* typwriter effect */
     @keyframes blink-caret {
+        from,
+        to {
+            border-color: transparent;
+        }
+        18% {
+            border-color: whitesmoke;
+        }
+    }
+    @keyframes blink-mobile {
         from,
         to {
             border-color: transparent;
